d078c9bc180d01eab787a0ca96f1e8b2000f3072
Sugakesshaa <sugakesshaa@pixelos.net>
2024-02-26 22:32:44 +0800

    Revert "sm6375-common: init: Dynamically set IRQ affinity for KGSL and MSM_DRM"
    This reverts commit 728ece077c316f6038e0d0c1f89566df544ca653.

--- init/init.blair.post_boot.sh
+++ init/init.blair.post_boot.sh
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2023 StatiXOS
-# Copyright (C) 2024 Paranoid Android
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -30,17 +29,3 @@ echo 1-2 > /dev/cpuset/audio-app/cpus
 # Sched
 echo 95 > /proc/sys/kernel/sched_upmigrate
 echo 85 > /proc/sys/kernel/sched_downmigrate
-
-function write_irq_affinity() {
-    # Arguments:
-    # $1 = irq name
-    # $2 = cpu id
-    irq_dir="$(dirname /proc/irq/*/$1)"
-    [ -d "$irq_dir" ] && echo $2 > "${irq_dir}/smp_affinity_list"
-}
-
-# IRQ Tuning
-# kgsl_3d0_irq -> CPU 1
-# msm_drm -> CPU 2
-write_irq_affinity kgsl_3d0_irq 1
-write_irq_affinity msm_drm 2
